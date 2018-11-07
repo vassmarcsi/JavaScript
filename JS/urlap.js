3af55ba5057ee356187be1e5bc29067aa4923f42
Maria Vass <vassmarcsi88@gmail.com>
2018-11-07 19:36:10 +0100

    Űrlap

--- JS/urlap.js
+++ JS/urlap.js
@@ -1,6 +1,30 @@
-function userCheck()
+function regForm()
 {
-    
-    
+    //ha elhagyom a var-t, akkor globális lesz a változó, ha eléírom, hogy let, akkor lokális
+    form = document.getElementsByTagName("input");
+    return form;    
 }
 
+function userCheck()
+{
+    document.getElementById("userNameError").innerHTML = "";
+    var userName = document.getElementById("username").value;
+    userName = userName.trim();
+    console.log(userName.length);
+    if (userName.length > 7 && userName.length < 26)
+    {
+        if (isFinite(userName[0]))
+        {
+            document.getElementById("userNameError").innerHTML = "Nem kezdődhet számmal";
+        }
+    }
+    else
+    {
+        document.getElementById("userNameError").innerHTML = "8 és 25 karakter közöttinek kell lennie vagy nem számmal kezdődik.";
+    }
+}
+
+function userCheck2(urlap)
+{
+    console.log(urlap[0].value);
+}
